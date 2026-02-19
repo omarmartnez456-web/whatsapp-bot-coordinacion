8f862ed863da37f043162f19189b3ef5bde37fbd
omarmartnez456-web <omarmartnez456@gmail.com>
2026-02-19 00:18:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-const express = require("express");
-const app = express();
-
-app.use(express.json());
-
-// Ruta principal
-app.get("/", (req, res) => {
-  res.send("Bot de WhatsApp funcionando 🚀");
-});
-
-// 🔐 Verificación del webhook (OBLIGATORIO para Meta)
 app.get("/webhook", (req, res) => {
 
   const VERIFY_TOKEN = "mi_token_123";
@@ -18,19 +7,8 @@ app.get("/webhook", (req, res) => {
   const challenge = req.query["hub.challenge"];
 
   if (mode === "subscribe" && token === VERIFY_TOKEN) {
-    console.log("Webhook verificado correctamente");
     return res.status(200).send(challenge);
   } else {
     return res.sendStatus(403);
   }
 });
-
-// 📩 Recibir mensajes
-app.post("/webhook", (req, res) => {
-  console.log("Mensaje recibido:", JSON.stringify(req.body, null, 2));
-  res.sendStatus(200);
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, ()
